42ca32914512d97fd6b3bf6084338aa1d3e04b8f
Akshat Shukla <akshatvns2006@gmail.com>
2026-08-03 15:03:33 +0530

    Time: 1 ms (98.06%), Space: 42.6 MB (45.51%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,21 +1,9 @@
 class Solution {
-    private int sumDigit(int num){
-
-        int sum = 0;
-
-        while(num>0){
-            int digit = num%10;
-            sum+=digit;
-            num/=10;
-        }
-        return sum;
+    public int addDigits(int num) {
+        if (num == 0)
+            return 0;
+        if (num % 9 == 0)
+            return 9;
+        return num % 9;
     }
-
-        public int addDigits(int num){
-            while(num>=10){
-                num=sumDigit(num);
-            }
-            return num;
-        }
-
-    }
+}

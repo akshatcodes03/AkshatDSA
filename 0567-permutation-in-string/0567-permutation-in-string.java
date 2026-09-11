f5ce3fbd07fbcd00e49c38e87a687e4443cc8ccf
Akshat Shukla <akshatvns2006@gmail.com>
2026-09-11 14:56:23 +0530

    Time: 6 ms (87.46%), Space: 44.1 MB (60.63%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,25 +1,49 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+
         int n1 = s1.length();
         int n2 = s2.length();
 
-        if(n2 < n1) return false;
+        int k = s1.length();
 
-        int[] count1 = new int[26];
-        int[] count2 = new int[26];
-
-        for(int i = 0; i < n1; i++){
-            count1[s1.charAt(i) - 'a']++;
-            count2[s2.charAt(i) - 'a']++;
+        if (n1 > n2) {
+            return false;
         }
 
-        if(Arrays.equals(count1, count2)) return true;
+        int[] fs1 = new int[128];
+        int[] fs2 = new int[128];
 
-        for(int i = n1; i < n2; i++){
-            count2[s2.charAt(i) - 'a']++;                 
-            count2[s2.charAt(i - n1) - 'a']--;          
+        int l = 0;
+        int r = 0;
 
-            if(Arrays.equals(count1, count2)) return true;
+        // First window
+        while (r < k) {
+            fs1[s1.charAt(r)]++;
+            fs2[s2.charAt(r)]++;
+            r++;
+        }
+
+        
+        if (Arrays.equals(fs1, fs2)) {
+            return true;
+        }
+
+       
+        while (r < n2) {
+
+            
+            fs2[s2.charAt(r)]++;
+
+           
+            fs2[s2.charAt(l)]--;
+
+            l++;
+            r++;
+
+            
+            if (Arrays.equals(fs1, fs2)) {
+                return true;
+            }
         }
 
         return false;
